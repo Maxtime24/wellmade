a71ce7b79e63ad4975d224b83721b80ba9d993d2
Maxtime24 <kimhyeongseop2008@gmail.com>
2026-02-16 23:45:39 +0900

    사이트 맵 정보 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://wellmade-art.co.kr'
+    const baseUrl = 'https://well-made.kr'
 
     return [
         {
